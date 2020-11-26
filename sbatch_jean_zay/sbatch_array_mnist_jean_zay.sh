ccea25c8d82326790dcd2d497e29363c1e057ad4
DejasDejas <julien.dejasmin@outlook.fr>
2020-11-26 14:41:30 +0100

    results sanity check

--- sbatch_jean_zay/sbatch_array_mnist_jean_zay.sh
+++ sbatch_jean_zay/sbatch_array_mnist_jean_zay.sh
@@ -15,7 +15,7 @@
 #BATCH --mail-type=ALL           
 #SBATCH --mail-user=julien.dejasmin@lis-lab.fr
 
-#SBATCH --array=1-12
+#SBATCH --array=1
 echo "$SLURM_ARRAY_TASK_ID"
 
 # nettoyage des modules charges en interactif et herites par defaut
