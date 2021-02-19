2feb41212e8195bac6400e2c1d6c8bc919fefd13
DejasDejas <julien.dejasmin@outlook.fr>
2021-02-19 09:12:39 +0100

    first result binary layer

--- sbatch_lis_cluster/sbatch_array_mnist.sh
+++ sbatch_lis_cluster/sbatch_array_mnist.sh
@@ -9,7 +9,7 @@
 #BATCH --mail-type=ALL                  # Mail notification of the events concerning the job : start time, end time,?~@?
 #SBATCH --mail-user=julien.dejasmin@lis-lab.fr
 
-#SBATCH --array=1-4%5   # % for run n jobs in same time
+#SBATCH --array=2-3%5   # % for run n jobs in same time
 echo "$SLURM_ARRAY_TASK_ID"
 
 LINE=$(sed -n "$SLURM_ARRAY_TASK_ID"p parameters_combinations/mnist_classifier_binary_exp.txt)
