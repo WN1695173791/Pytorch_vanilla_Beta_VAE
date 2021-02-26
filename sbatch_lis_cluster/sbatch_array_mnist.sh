971e1f1654716bda31bb3fba60a7d8975924c638
DejasDejas <julien.dejasmin@outlook.fr>
2021-02-26 16:44:57 +0100

    re

--- sbatch_lis_cluster/sbatch_array_mnist.sh
+++ sbatch_lis_cluster/sbatch_array_mnist.sh
@@ -2,14 +2,14 @@
 #SBATCH --job-name="VAE"      # Job Name
 #SBATCH --partition=gpu                  # Name of the Slurm partition used
 #SBATCH --gres=gpu:1                # nombre de GPU a reserver
-#SBATCH --time=00:30:00                # time (DD-HH:MM)
+#SBATCH --time=00:15:00                # time (DD-HH:MM)
 #SBATCH --output=output_exp/lis_cluster/mnist_%A_%a.out       # STDOUT
 #SBATCH --error=output_exp/lis_cluster/mnist_%A_%a.err       # STDERR
 
 #BATCH --mail-type=ALL                  # Mail notification of the events concerning the job : start time, end time,?~@?
 #SBATCH --mail-user=julien.dejasmin@lis-lab.fr
 
-#SBATCH --array=29-36%10  # % for run n jobs in same time
+#SBATCH --array=10-18  #29-36%10  # % for run n jobs in same time
 echo "$SLURM_ARRAY_TASK_ID"
 
 LINE=$(sed -n "$SLURM_ARRAY_TASK_ID"p parameters_combinations/mnist_classifier_ratio.txt)
