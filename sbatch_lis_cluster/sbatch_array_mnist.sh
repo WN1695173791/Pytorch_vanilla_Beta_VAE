5fc277d60d8196a4c64eae19a0db1da376d26087
DejasDejas <julien.dejasmin@outlook.fr>
2021-03-01 09:20:39 +0100

    re

--- sbatch_lis_cluster/sbatch_array_mnist.sh
+++ sbatch_lis_cluster/sbatch_array_mnist.sh
@@ -9,7 +9,8 @@
 #BATCH --mail-type=ALL                  # Mail notification of the events concerning the job : start time, end time,?~@?
 #SBATCH --mail-user=julien.dejasmin@lis-lab.fr
 
-#SBATCH --array=10-18  #29-36%10  # % for run n jobs in same time
+#SBATCH --array=29-36%10  # 10-18 % for run n jobs in same time
+
 echo "$SLURM_ARRAY_TASK_ID"
 
 LINE=$(sed -n "$SLURM_ARRAY_TASK_ID"p parameters_combinations/mnist_classifier_ratio.txt)
