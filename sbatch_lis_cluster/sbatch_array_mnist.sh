c82eed150de123de20899bf7a485ed2b6b310456
DejasDejas <julien.dejasmin@outlook.fr>
2021-03-15 09:43:52 +0100

    update ratio lis

--- sbatch_lis_cluster/sbatch_array_mnist.sh
+++ sbatch_lis_cluster/sbatch_array_mnist.sh
@@ -9,7 +9,7 @@
 #BATCH --mail-type=ALL                  # Mail notification of the events concerning the job : start time, end time,?~@?
 #SBATCH --mail-user=julien.dejasmin@lis-lab.fr
 
-#SBATCH --array=2-5  # % for run n jobs in same time
+#SBATCH --array=2-8  # % for run n jobs in same time
 
 echo "$SLURM_ARRAY_TASK_ID"
 
