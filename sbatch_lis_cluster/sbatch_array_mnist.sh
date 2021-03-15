e7377a7e1c2843d6019b9aa77a3cc89cbb8bbdc6
DejasDejas <julien.dejasmin@outlook.fr>
2021-03-15 18:22:27 +0100

    re

--- sbatch_lis_cluster/sbatch_array_mnist.sh
+++ sbatch_lis_cluster/sbatch_array_mnist.sh
@@ -9,7 +9,7 @@
 #BATCH --mail-type=ALL                  # Mail notification of the events concerning the job : start time, end time,?~@?
 #SBATCH --mail-user=julien.dejasmin@lis-lab.fr
 
-#SBATCH --array=2-6  # % for run n jobs in same time
+#SBATCH --array=2-2  # 6  # % for run n jobs in same time
 
 echo "$SLURM_ARRAY_TASK_ID"
 
