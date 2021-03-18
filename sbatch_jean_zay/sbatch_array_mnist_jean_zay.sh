594d3ae4cb19214df18020b552a6620e1a34b5c2
DejasDejas <julien.dejasmin@outlook.fr>
2021-03-18 08:22:12 +0100

    re jean zay

--- sbatch_jean_zay/sbatch_array_mnist_jean_zay.sh
+++ sbatch_jean_zay/sbatch_array_mnist_jean_zay.sh
@@ -14,7 +14,7 @@
 #BATCH --mail-type=ALL           
 #SBATCH --mail-user=julien.dejasmin@lis-lab.fr
 
-#SBATCH --array=68-68  # -187
+#SBATCH --array=2-187
 echo "$SLURM_ARRAY_TASK_ID"
 
 # nettoyage des modules charges en interactif et herites par defaut
