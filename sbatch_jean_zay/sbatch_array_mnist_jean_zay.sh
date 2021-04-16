ea26896bbdf9d9519b96c1c5372f942e749593fd
DejasDejas <julien.dejasmin@outlook.fr>
2021-04-16 15:39:08 +0200

    re

--- sbatch_jean_zay/sbatch_array_mnist_jean_zay.sh
+++ sbatch_jean_zay/sbatch_array_mnist_jean_zay.sh
@@ -4,7 +4,7 @@
 #SBATCH --qos=qos_gpu-t4             # for jean-zay
 #SBATCH --gres=gpu:1                # nombre de GPU a reserver
 #SBATCH --cpus-per-task=3          # nombre de coeurs CPU par tache
-#SBATCH --time=00:30:00                  # time (DD-HH:MM)
+#SBATCH --time=01:30:00                  # time (DD-HH:MM)
 #SBATCH --output=output_exp/jean_zay/mnist_%A_%a.out       # STDOUT
 #SBATCH --error=output_exp/jean_zay/mnist_%A_%a.err       # STDERR
 
@@ -14,7 +14,7 @@
 #BATCH --mail-type=ALL           
 #SBATCH --mail-user=julien.dejasmin@lis-lab.fr
 
-#SBATCH --array=1-3  # 6-36  #  1h30
+#SBATCH --array=6-36 
 echo "$SLURM_ARRAY_TASK_ID"
 
 # nettoyage des modules charges en interactif et herites par defaut
